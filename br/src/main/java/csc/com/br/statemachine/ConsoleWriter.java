291837ae0dad2dd7b1cb207283a1b38c4fd8b876
Vasilena Dragancheva <vasilena86@hotmail.com>
2016-06-07 00:19:05 +0300

    StateMachin Upodate

--- br/src/main/java/csc/com/br/statemachine/ConsoleWriter.java
+++ br/src/main/java/csc/com/br/statemachine/ConsoleWriter.java
@@ -1,5 +1,10 @@
 package csc.com.br.statemachine;
 
-public class ConsoleWriter {
+public class ConsoleWriter implements IWriter {
+
+	public void Writer(String output) {
+		System.out.println(output);
+		
+	}
 
 }
--- br/src/main/java/csc/com/br/statemachine/IWriter.java
+++ br/src/main/java/csc/com/br/statemachine/IWriter.java
@@ -1,5 +1,7 @@
 package csc.com.br.statemachine;
 
 public interface IWriter {
-
+	
+	//object of type Output! not string;
+     void Writer(String output);
 }
