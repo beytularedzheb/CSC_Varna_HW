bcde0b9eb3085a5bed31c2763e6a6928ec56e502
Vasilena Dragancheva <vasilena86@hotmail.com>
2016-06-06 22:48:36 +0300

    ConsoleDataProvider
    reformated

--- br/src/main/java/csc/com/br/dataprovider/ConsoleDataProvider.java
+++ br/src/main/java/csc/com/br/dataprovider/ConsoleDataProvider.java
@@ -1,6 +1,7 @@
 package csc.com.br.dataprovider;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,28 +12,32 @@ import csc.com.br.statemachine.Identity;
 import csc.com.br.statemachine.State;
 
 public class ConsoleDataProvider implements IDataProvider {
+	private final Scanner scanner = new Scanner(System.in);
+	private final HashSet<IState> states = new HashSet<IState>();
+	private final IState initialState;
+
+	public ConsoleDataProvider(IState initialState) {
+		this.initialState = initialState;
+		this.states.add(initialState);
+	}
 
 	public IState getInitialState() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.initialState;
 	}
 
 	public List<ICommand> getProgram() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void getMachine() {
-		Map<IState, HashMap<ICommand, IState>> machine = new HashMap<IState, HashMap<ICommand, IState>>();
-		Scanner scn = new Scanner(System.in);
-		String line = scn.nextLine();
-		String[] tokens = line.split("\\s+", 3);
 
-		State mainState = new State(new Identity(tokens[0]));
-		State nextState = new State(new Identity(tokens[1]));
-		//ICommand command = new Command(token[2]);
-		
-		//mainState.addNextState(command, nextState);
+		String line;
+		while (this.scanner.hasNextLine()) {
+			line = this.scanner.nextLine();
+			String[] tokens = line.split("\\s+", 3);
+
+		}
+
 	}
 
 }
